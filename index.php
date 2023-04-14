a152728f38fa2416c6100f866460205141f93dcf
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-04-14 12:21:12 +0530

    temp

--- index.php
+++ index.php
@@ -4,13 +4,16 @@ echo "Hi";
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HarvestHub</title>
-  </head>
-  <body>
-    <h1></h1>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HarvestHub</title>
+</head>
+
+<body>
+  <h1></h1>
+</body>
+
 </html>
