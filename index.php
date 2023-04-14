75f936bb968fdf6d3eb5ba528a2016486686df0e
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2023-04-14 15:16:59 +0530

    search bar added

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 
 <body>
   <?php include('templates/navbar.php'); ?>
+  <div class="wrapper">
+  <input type="text" placeholder="Vegetables/Fruits">
+  <input type="submit" value="Search"/>
+  <div class="cards">
+
+</div>
+</div>
 </body>
 
 </html>
