39015b2f9a2153b16a6120c55d992dd398895205
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2023-04-14 13:41:09 +0530

    image url added

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-echo "Hi";
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="cart.css">
-  <title>HarvestHub</title>
-</head>
+
 
 <body>
   <?php include('templates/navbar.php'); ?>
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,9 +1,16 @@
 <!-- logo                                       name cart logout -->
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles\index.css">
+  <title>HarvestHub</title>
+</head>
 
 <nav>
-<div class="logo">
 
-</div>
+<img src="assets\logo.png" class="logo"/>
+
 <div class="details">
 name
 cart 
