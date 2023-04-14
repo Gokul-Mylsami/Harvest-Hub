a218e7dad07e7885a81600d18d44dd1c8a9d8e60
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-04-14 12:50:40 +0530

    dummy data's

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "Hi";
+
 ?>
 
 <!DOCTYPE html>
---
+++ utils/connect_db.php
@@ -0,0 +1,3 @@
+<!-- products : id(pk), name, price, qtyleft, description,image  -->
+<!--  -->
+<!-- user : name, password, email(pk), cartitems [{id:product_id,noOfProduct:2}] -->
