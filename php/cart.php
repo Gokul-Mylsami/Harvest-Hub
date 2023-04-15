8f973b7f559feb057534c9bcbc8fa19478c8a365
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-04-15 18:45:01 +0530

    card functionality

--- php/cart.php
+++ php/cart.php
@@ -9,18 +9,42 @@ $response = array(
 );
 
 $_userEmail = 'abc@gmail.com';
+
+$products = array();
+$sql = "SELECT * FROM products";
+$result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $products[] = $row;
+}
 if(isset($_POST['action']) && $_POST['action'] === 'get'){
-    
-    $sql = "SELECT cartitems FROM user WHERE email = '$_userEmail'";
+    $sql = "SELECT cartitems FROM user WHERE email = '$_userEmail'"; 
     $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row['cartitems']!=null) {
         $cartitems = $row['cartitems'];
         $cartitems = json_decode($cartitems, true);
+        $data = array();
+        foreach($cartitems as $cartitem){
+            foreach($products as $product)
+            {
+                if($cartitem['id'] == $product['id']){
+                    $data[] = array(
+                        'id' => $product['id'],
+                        'name' => $product['name'],
+                        'price' => $product['price'],
+                        'image' => $product['image'],
+                        'description' => $product['description'],
+                        'qtyleft' => $product['qtyleft'],
+                        'quantity' => $cartitem['quantity'],
+                    );
+                }
+            }
+        }
         $response['status'] = 'success';
         $response['status_code'] = '200';
         $response['message'] = 'Cart items fetched successfully';
-        $response['data'] = $cartitems;
+        $response['data'] = $data;
     } else {
         $response['status'] = 'error';
         $response['status_code'] = '404';
@@ -32,10 +56,6 @@ if(isset($_POST['action']) && $_POST['action'] === 'add'){
     $_id = $_POST['id'];
     $_name = $_POST['name'];
 
-    $sql = "SELECT * FROM products WHERE id = '$_id'";
-    $result = mysqli_query($conn, $sql);
-    $product = mysqli_fetch_assoc($result);
-
     $sql = "SELECT cartitems FROM user WHERE email = '$_userEmail'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
@@ -43,7 +63,6 @@ if(isset($_POST['action']) && $_POST['action'] === 'add'){
     $cartitems = $row['cartitems'];
 
     if($cartitems === null){
-        //update the user table cart items to [{id:1,noOfProducts:1}]
         $user_products[] = array(
             'id' => $_id,
             'quantity' => 1,
@@ -53,11 +72,26 @@ if(isset($_POST['action']) && $_POST['action'] === 'add'){
         
         $sql = "UPDATE user SET cartitems = '$user_products' WHERE email = '$_userEmail'";
         $result = mysqli_query($conn, $sql);
+        
+        $data = array();
+        
+
         if($result){
+
+            $data[] = array(
+                'id' => $_id,
+                'name' => $_name,
+                'price' => $product['price'],
+                'image' => $product['image'],
+                'description' => $product['description'],
+                'qtyleft' => $product['qtyleft'],
+                'quantity' => 1,
+            );
+
             $response['status'] = 'success';
             $response['status_code'] = '200';
             $response['message'] = 'Cart items added successfully';
-            $response['data'] = $user_products;
+            $response['data'] = $data;
         } else {
             $response['status'] = 'error';
             $response['status_code'] = '500';
@@ -66,6 +100,18 @@ if(isset($_POST['action']) && $_POST['action'] === 'add'){
     } else {
         $cartitems = json_decode($cartitems, true);
 
+        foreach($cartitems as $item)
+        {
+
+            if($item['id'] === $_id)
+            {
+                $response['status'] = 'error';
+                $response['status_code'] = '500';
+                $response['message'] = 'Product already exists in cart';
+                echo json_encode($response);
+                exit();
+            }
+        }
 
         $newCartItems = array(
             'id' => $_id,
