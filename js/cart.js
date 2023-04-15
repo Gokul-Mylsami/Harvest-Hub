cf064db57a671457054b393c6a4bb3e8b3516530
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2023-04-15 19:19:33 +0530

    error

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,64 @@
 let cartItems;
+
+const itemsWrapper = document.querySelector(".items-wrapper");
 const getCartItems = async () => {
   let formData = { action: "get" };
   $.ajax({
     type: "POST",
     url: "../php/cart.php",
     data: formData,
-    success: function (response) {
+    success: function(response) {
       let data = JSON.parse(response);
       console.log(data);
+      cartItems = data;
+    
       //   upadateUI(data.data);
     },
-    error: function (jqXHR, textStatus, errorThrown) {
+    error: function(jqXHR, textStatus, errorThrown) {
       console.log(errorThrown);
     },
   });
 };
 
+const displayData = (data) => {
+  data.map((item) => {
+    let template = `<tr class="prod-row">
+    <td class="tab-col">
+<div class="prod-details-col">
+    <img src='${item.image}' class="prod-img"/>
+      <div class="prod-details">
+                    <p class="prod-name">'${item.name}'</p>
+                    <p class="prod-desc">
+                      '${item.description}'
+                    </p>
+                    <p class="prod-remove">Remove</p>
+                  </div>
+                </div>
+              </td>
+<td class="tab-col">
+                <div class="quantity-col">
+                  <button>+</button>
+                  <p>'${item.quantity}'</p>
+                  <button>-</button>
+                </div>
+              </td>
+              <td class="tab-col">
+                <p class="prod-price">₹ '${item.price}'</p>
+              </td>
+              <td>
+                <p class="prod-total">₹ '${item.quantity * item.price}'</p>
+              </td>
+            </tr>
+`;
+    itemsWrapper.innerHTML += template;
+  });
+};
+const renderData = async () => {
+  const data = await getCartItems();
+  // displayData(data.data);
+  console.log(cartItems);
+};
+
 const upadateUI = (data) => {};
 
-getCartItems();
+renderData();
