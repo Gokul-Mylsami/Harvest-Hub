0beca8165cd7b0ce7447d1a471547fb469d5ca3d
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2023-04-15 18:46:39 +0530

    notification added

--- js/index.js
+++ js/index.js
@@ -19,11 +19,11 @@ const addToCart = async (id, name) => {
     type: "POST",
     url: "../php/cart.php",
     data: data,
-    success: function (response) {
+    success: function(response) {
       let res = JSON.parse(response);
       console.log(res);
     },
-    error: function (jqXHR, textStatus, errorThrown) {
+    error: function(jqXHR, textStatus, errorThrown) {
       console.log(errorThrown);
     },
   });
@@ -58,8 +58,8 @@ const displayData = (data) => {
     button.addEventListener("click", (event) => {
       const productId = event.target.parentNode.getAttribute("key");
       //get the product name
-      const productName =
-        event.target.parentNode.querySelector(".product-name").innerText;
+      const productName = event.target.parentNode.querySelector(".product-name")
+        .innerText;
       console.log("Product added to cart:", productId, productName);
       addToCart(productId, productName);
     });
@@ -73,3 +73,32 @@ const renderData = async () => {
 };
 
 renderData();
+
+function showNotification(message, status) {
+  var notification = document.createElement("div");
+  notification.innerText = message;
+  notification.style.position = "fixed";
+  notification.style.bottom = "-50px";
+  notification.style.left = "50%";
+  notification.style.transform = "translateX(-50%)";
+  notification.style.background = status === "success" ? "#4CAF50" : "#f44336";
+  notification.style.color = "#fff";
+  notification.style.padding = "10px";
+  notification.style.borderRadius = "5px";
+  notification.style.opacity = "0";
+  document.body.appendChild(notification);
+  setTimeout(function() {
+    notification.style.bottom = "20px";
+    notification.style.opacity = "1";
+    notification.style.transition = "bottom 0.3s ease-in-out";
+  }, 100);
+  setTimeout(function() {
+    notification.style.bottom = "-50px";
+    notification.style.opacity = "0";
+    notification.style.transition =
+      "bottom 0.3s ease-in-out, opacity 0.5s ease-out";
+    setTimeout(function() {
+      document.body.removeChild(notification);
+    }, 500);
+  }, 3000);
+}
